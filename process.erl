854166892b6c3b1bb05d0d38c11ae4878b6fcf75
Tyler Boyd <tboyd47@new-host.home>
2012-12-11 16:58:01 -0500

    add methods back

--- process.erl
+++ process.erl
@@ -3,6 +3,10 @@
 
 start(Data) ->
   receive
+    {get, Pid} ->
+      io:format("sending data: ~p~n", [Data]),
+      Pid ! Data,
+      start(Data);
     {get, Pid, Key} ->
       io:format("sending data @ key: ~p~n", [Key]),
       case lists:keyfind(Key, 1, tuple_to_list(Data)) of
@@ -10,6 +14,10 @@ start(Data) ->
         false -> Pid ! null
       end,
       start(Data);
+    {set, Pid, Data2} ->
+      io:format("setting data: ~p~n", [Data2]),
+      Pid ! {Data, Data2},
+      start(Data2);
     {set, Pid, Key, Val} ->
       io:format("setting data @ key: ~p~n", [Key]),
       Result = lists:keydelete(Key, 1, tuple_to_list(Data)),
